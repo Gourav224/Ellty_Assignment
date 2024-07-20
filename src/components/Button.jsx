e3d01ef79a7029298389f71a964099c114a9536d
Gourav224 <gkrdhiman@gmail.com>
2024-07-20 12:30:12 +0530

    button completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,17 @@
 
-const Button = ({text="Done"}) => {
+const Button = ({ text = "Done" }) => {
+    const handleClick = () => {
+        alert('Button clicked!');
+    };
+
     return (
-        <button className="mt-6 w-full bg-[#ffce22] text-white py-2 rounded-md hover:bg-[#ffd84d]">
+        <button
+            className="mt-6 w-full bg-[#ffce22] text-white py-2 rounded-md hover:bg-[#ffd84d]"
+            onClick={handleClick}
+        >
             {text}
         </button>
     );
 };
 
 export default Button;
-
